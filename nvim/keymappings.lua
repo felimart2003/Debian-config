1356063d187154fd72e2a1927bc7ed5fc9eed693
Felipe Martinez <felimart2003@gmail.com>
2024-04-27 17:13:42 -0400

    add nvim

---
+++ nvim/keymappings.lua
@@ -0,0 +1,10 @@
+-- MAPPINGS FOR COLEMAK
+-- Remap j to n
+--vim.api.nvim_set_keymap('n', 'j', 'n', { noremap = true, silent = true })
+
+-- Remap k to e
+--vim.api.nvim_set_keymap('n', 'k', 'e', { noremap = true, silent = true })
+
+-- Remap l to i
+--vim.api.nvim_set_keymap('n', 'l', 'i', { noremap = true, silent = true })
+
